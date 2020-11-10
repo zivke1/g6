4778a05ccada6a628d771bcbfb271910cb4db553
zivke <zivke@10.0.0.6>
2020-11-10 19:35:50 +0200

    hh

--- g/src/a/sss.java
+++ g/src/a/sss.java
@@ -1,10 +1,10 @@
 package a;
 
 public class sss {
-	private int dd;
-
-	public void Print() {
-		System.out.println("This is Git");
-	String idan_bar ="Idan Bar accelerate relationships ";	 
+private int dd;
+	public void Haalid()
+	{
+		System.out.println("Who let the Haalid out");
+		//dddd,,,,,,jjjjjjjj
 	}
 }
