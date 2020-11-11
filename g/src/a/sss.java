9389125d7aca30163f53122d2f06db54871f71a5
zivke <zivke@10.0.0.6>
2020-11-11 20:36:40 +0200

    z

--- g/src/a/sss.java
+++ g/src/a/sss.java
@@ -1,5 +1,18 @@
 package a;
 
 public class sss {
-private int dd;
+	private int dd;
+
+	public void Print() {
+		System.out.println("This is Git");
+		System.out.println("This is eliran");
+//hgvhgvj
+		//this is eliranjjjjjjjjjjjjjjjjjjjjjjjjjsaaaaaaaaaaaaaaaaaaas
+	}
+	public void Haalid()
+	{
+		System.out.println("Who let the Haalid out");
+		//ddddnnnnjjjjjjjj
+	}
+
 }
