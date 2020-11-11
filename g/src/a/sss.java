d448a8561b66edf4c2e84cc3abfa69c7654868b1
zivke <zivke@10.0.0.6>
2020-11-11 20:23:07 +0200

    jjjjkkkk

--- g/src/a/sss.java
+++ g/src/a/sss.java
@@ -9,4 +9,33 @@ public class sss {
 //hgvhgvj
 		//this is eliranjjjjjjjjjjjjjjjjjjjjjjjjj
 	}
+	public void Haalid()
+	{
+		System.out.println("Who let the Haalid out");
+		//ddddnnnnjjjjjjjj
+	}
+	private int dd;
+
+	public void Print() {
+		System.out.println("This is Git");
+		System.out.println("This is eliran");
+//hgvhgvj
+		//this is eliranjjjjjjjjjjjjjjjjjjjjjjjjj
+	}
+	private int dd;
+
+	public void Print() {
+		System.out.println("This is Git");
+		System.out.println("This is eliran");
+//hgvhgvj
+		//this is eliranjjjjjjjjjjjjjjjjjjjjjjjjj
+	}
+	private int dd;
+
+	public void Print() {
+		System.out.println("This is Git");
+		System.out.println("This is eliran");
+//hgvhgvj
+		//this is eliranjjjjjjjjjjjjjjjjjjjjjjjjj
+	}
 }
