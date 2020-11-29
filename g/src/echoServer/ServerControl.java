a5708e43ebd864eeaaf0842c6a7bbef56003924b
zivke1 <63557865+zivke1@users.noreply.github.com>
2020-11-29 18:17:57 +0200

    push server

---
+++ g/src/echoServer/ServerControl.java
@@ -0,0 +1,59 @@
+package echoServer;
+
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+
+public class ServerControl {
+
+    @FXML
+    private GridPane statusLabel;
+
+
+    @FXML
+    private Text ipLabel;
+
+    @FXML
+    private Text hostLabel;
+
+    @FXML
+     private TextField statusState;
+
+    @FXML
+    private TextField ipState;
+
+    @FXML
+     private TextField hostState;
+
+    EchoServer echoServer;
+    @FXML
+    void startServer(MouseEvent event) {
+    	try {
+    	echoServer= new EchoServer(5555 ,this);
+        try 
+        {
+        echoServer.listen(); //Start listening for connections
+        } 
+        catch (Exception ex) 
+        {
+          System.out.println("ERROR - Could not listen for clients!");
+        }
+    	}catch(Exception e) {
+    		e.printStackTrace();
+    	}
+   
+    }
+    
+    @FXML
+      public void setParameters(String ip ,String host,String status) {
+    	statusState.setText(status);
+    	ipState.setText(ip);
+    	hostState.setText(host);
+    }
+    
+
+}
---
+++ g/src/echoServer/ServerMain.java
@@ -0,0 +1,41 @@
+package echoServer;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+//hii
+
+public class ServerMain extends Application{
+
+	 BorderPane root;
+	
+	public void start(Stage stage) {
+		
+		Scene scene;
+
+		ServerControl controller;
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource("/fxmlFiles/Server.fxml"));
+			root = loader.load();/////////////////
+		
+			controller = loader.getController();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		}
+		scene=new Scene(root);
+		stage.setScene(scene);
+		stage.setTitle("server");
+		stage.show();
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
