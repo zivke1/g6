b49454f550b93d46610ee37c5a8afb5033a45125
Nitzan Smaga <nitzansm92@gmail.com>
2020-12-01 10:32:38 +0200

    added id check function for the show details and update email

--- g/src/clientTry/EnterIDController.java
+++ g/src/clientTry/EnterIDController.java
@@ -18,53 +18,55 @@ public class EnterIDController {
 	public static String iD;
 	@FXML
 	private TextField id;
-    
+
 	@FXML
 	void ShowDetails(MouseEvent event) {
-
-		ArrayList<String> arr = new ArrayList<String>();
-		arr.add("showTable");
-		arr.add(id.getText());
-		((Node) event.getSource()).getScene().getWindow().hide();
-
 		UserInformationController cT = new UserInformationController();
-		try {
-			cT.showDetails(arr);
-		} catch (Exception e) {
-			e.printStackTrace();
+		if (cT.CheckID(id.getText())) {
+			ArrayList<String> arr = new ArrayList<String>();
+			arr.add("showTable");
+			arr.add(id.getText());
+			((Node) event.getSource()).getScene().getWindow().hide();
+			try {
+				cT.showDetails(arr);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
 		}
-
+		// else show error id does not exist
 	}
-	
 
-    
 	@FXML
 	void UpdateEmail(MouseEvent event) {
-		((Node) event.getSource()).getScene().getWindow().hide();
-		Stage primaryStage = new Stage();
-		
-		//this make the X btn to close the connection
-			primaryStage.setOnCloseRequest(evt->{
-	    	ArrayList<String> closeArrayList = new ArrayList<String>();
-	    	closeArrayList.add("close");
-			ClientMain.chat.accept(closeArrayList);
-	    	ClientMain.chat.stopConnection();
-		});
-		
-		//TODO add query to check if the id exist
-	    FXMLLoader loader=new FXMLLoader();
-		VBox root = null;
-		try {
-			root = loader.load(getClass().getResource("/clientTry/UpdateEmail.fxml").openStream());
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-		iD=id.getText();
-		Scene scene = new Scene(root);
-		scene.getStylesheets().add(getClass().getResource("/clientTry/application.css").toExternalForm());
-		primaryStage.setTitle("Update Email");
-		primaryStage.setScene(scene);
-		primaryStage.show();
-	}
+		UserInformationController cT = new UserInformationController();
+		if (cT.CheckID(id.getText())) {
+			((Node) event.getSource()).getScene().getWindow().hide();
+			Stage primaryStage = new Stage();
 
+			// this make the X btn to close the connection
+			primaryStage.setOnCloseRequest(evt -> {
+				ArrayList<String> closeArrayList = new ArrayList<String>();
+				closeArrayList.add("close");
+				ClientMain.chat.accept(closeArrayList);
+				ClientMain.chat.stopConnection();
+			});
+
+			FXMLLoader loader = new FXMLLoader();
+			VBox root = null;
+			try {
+				root = loader.load(getClass().getResource("/clientTry/UpdateEmail.fxml").openStream());
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+			iD = id.getText();
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("/clientTry/application.css").toExternalForm());
+			primaryStage.setTitle("Update Email");
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		}
+		// else show error id does not exist
+
+	}
 }
