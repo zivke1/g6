7679914af543389c8525a04282c3fe095afdb243
zivke1 <63557865+zivke1@users.noreply.github.com>
2020-12-08 14:35:01 +0200

    aa

---
+++ g/src/clientTry/CheckAvailabilityController.java
@@ -0,0 +1,41 @@
+package clientTry;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
+public class CheckAvailabilityController {
+
+    @FXML
+    private ImageView imgContactUs;
+
+    @FXML
+    private Button backBtn;
+
+    @FXML
+    private Button helpBtn;
+
+    @FXML
+    private Text parkNameText;
+
+    @FXML
+    private Text numberToFullCapacitiy;
+
+    @FXML
+    void backClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void goToContactUsPopUp(MouseEvent event) {
+
+    }
+
+    @FXML
+    void helpBtnPressed(MouseEvent event) {
+
+    }
+
+}
