85c39c356d966260d0bbab9500bf61c149848787
zivke1 <63557865+zivke1@users.noreply.github.com>
2020-12-09 21:35:35 +0200

    ziv work on login controller

--- g/src/clientTry/ClientMain.java
+++ g/src/clientTry/ClientMain.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -18,7 +19,7 @@ public class ClientMain extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		chat=new ClientConsole("localhost", 5555);
-		//this make the X btn to close the connection
+//		this make the X btn to close the connection
 		primaryStage.setOnCloseRequest(evt->{
 			if (ClientMain.chat.checkConnection()) {
 	    	ArrayList<String> arr = new ArrayList<String>();
@@ -27,20 +28,39 @@ public class ClientMain extends Application {
 	    	ClientMain.chat.stopConnection();
 			}
 		});
-		VBox vbox = null;
-		EnterIDController controller;
+//		VBox vbox = null;
+//		EnterIDController controller;
+//		try {
+//			FXMLLoader loader=new FXMLLoader();
+//			loader.setLocation(getClass().getResource("EnterID.fxml"));
+//			vbox = loader.load();
+//			controller = loader.getController();
+//		} catch (IOException e) {
+//		e.printStackTrace();
+//		}	
+//		Scene scene=new Scene(vbox);
+//		primaryStage.setScene(scene);
+//		primaryStage.setTitle("Enter ID");
+//		primaryStage.show();
+		
+		
+		/////////////////////////////////////////////////////type name = new type();
+		BorderPane borderPane = null;
+		//EnterIDController controller;
 		try {
 			FXMLLoader loader=new FXMLLoader();
-			loader.setLocation(getClass().getResource("EnterID.fxml"));
-			vbox = loader.load();
-			controller = loader.getController();
+
+			loader.setLocation(getClass().getResource("../fxmlFiles/LoginP.fxml"));
+			borderPane = loader.load();
+			//controller = loader.getController();
 		} catch (IOException e) {
 		e.printStackTrace();
 		}	
-		Scene scene=new Scene(vbox);
+		Scene scene=new Scene(borderPane);
 		primaryStage.setScene(scene);
-		primaryStage.setTitle("Enter ID");
+		primaryStage.setTitle("Login");
 		primaryStage.show();
+		/////////////////////////////////////////////////
 	}
 
 }
