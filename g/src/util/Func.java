bd1217fc13e0046a6583bad42ef9b26fa703f4e2
Nitzan Smaga <nitzansm92@gmail.com>
2020-12-27 15:44:31 +0200

    added unfix date and fix date string

--- g/src/util/Func.java
+++ g/src/util/Func.java
@@ -12,11 +12,22 @@ public class Func {
 		return d;
 	}
 	
+	public static String fixDateString(String date)
+	{
+		String year=date.substring(0, 4),month=date.substring(5, 7),day=date.substring(8, 10),res=day+"-"+month+"-"+year;
+		return res;
+	}
+	
+	public static String unFixDate(String date)
+	{
+		String day=date.substring(0, 2),month=date.substring(3, 5),year=date.substring(6, 10),res=year+"-"+month+"-"+day;
+		return res;
+	}
 	public static void main(String[] args) {
 		Date d=new Date();
-		Calendar c=Calendar.getInstance();
-		SimpleDateFormat dateOnly = new SimpleDateFormat("dd/MM/yyyy");
-		System.out.println(dateOnly.format(c.getTime()));
-		//System.out.println(fixDate(d));
+//		Calendar c=Calendar.getInstance();
+//		SimpleDateFormat dateOnly = new SimpleDateFormat("dd/MM/yyyy");
+//		System.out.println(dateOnly.format(c.getTime()));
+		System.out.println(fixDateString(unFixDate(fixDate(d))));
 	}
 }
