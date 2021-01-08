fe339627ea2e7e31f5f6386a98ec882aeb11f8ff
Nitzan Smaga <nitzansm92@gmail.com>
2021-01-08 14:57:29 +0200

    fix

--- Server/src/echoServer/SimulationController.java
+++ Server/src/echoServer/SimulationController.java
@@ -62,13 +62,13 @@ public class SimulationController {
 		errorMsg.setText(mysqlConnection.CancelOrder(arr));
 		cofirmBtn.setVisible(false);
 		backBtn.setVisible(false);
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		((Node) event.getSource()).getScene().getWindow().hide();
+//		try {
+//			Thread.sleep(1000);
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		((Node) event.getSource()).getScene().getWindow().hide();
 	}
 
 	@FXML
@@ -78,13 +78,13 @@ public class SimulationController {
 		errorMsg.setText(mysqlConnection.WaitingForVisitOrder(arr));
 		cofirmBtn.setVisible(false);
 		backBtn.setVisible(false);
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		((Node) event.getSource()).getScene().getWindow().hide();
+//		try {
+//			Thread.sleep(1000);
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		//((Node) event.getSource()).getScene().getWindow().hide();
 	}
 
 	@FXML
